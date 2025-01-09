ab34cbf323fbfda5de609b4d94ec7cb1585a964b
unknown <gditrani269@gmail.com>
2025-01-09 16:13:18 -0300

    se agrega ej de paginado en test-db.py

--- test-db.py
+++ test-db.py
@@ -1,18 +1,32 @@
 import mysql.connector
+
+def fPaginado ():
+    print ("funcion de paginado")
+    cursor = conex.cursor()
+    sql = "select * FROM History limit 10 offset 10"
+    cursor.execute(sql)
+    results = cursor.fetchall ()
+    print(results)
+    print ("sgunda pagina")
+    sql = "select * FROM History limit 10 offset 20"
+    cursor.execute(sql)
+    results = cursor.fetchall ()
+    print(results)
+
 print ("otra prueba db")
 
 dbConnect2 = {
     'host':'172.28.183.229',
     'user':'root',
     'password':'sasa1234',
-    'database':'test',
+    'database':'retest',
  }
 
 conex = mysql.connector.connect(**dbConnect2)
 
 cursor = conex.cursor()
 
-sql = "select * from acciones"
+sql = "select * from History"
 
 cursor.execute(sql)
 
@@ -29,3 +43,13 @@ databases = ("show databases")
 cursor2.execute(databases)
 for (databases) in cursor2:
      print (databases[0])
+
+print ("------------------------")
+sql = "select * FROM History limit 10 offset 10"
+cursor = conex.cursor()
+cursor.execute(sql)
+#cursor3 = conex.cursor()
+results = cursor.fetchall ()
+print(results)
+
+fPaginado ()
